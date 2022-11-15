dafc30d655e5890081d4825ded59b7b85f850ee7
montejs3 <98852422+montejs3@users.noreply.github.com>
2022-11-14 21:51:03 -0500

    sql

--- AforoAndes/aforoandes/doc/Docs/SQL_Requerimientos/RF15.sql
+++ AforoAndes/aforoandes/doc/Docs/SQL_Requerimientos/RF15.sql
@@ -1,19 +1,2 @@
-DECLARE
-    canprod int := 1;
-    compraid number := 230942;
-    productoid number := 12;
-    promoid number := 12323;
-
-INSERT INTO Producto_Compra 
-VALUES(canprod, compraid, productoid, promoid)
-
-UPDATE Productos
-SET p.EXISTENCIAS_ESTANTE = p.EXISTENCIAS_ESTANTE - pc.CANTIDAD_PRODUCTO
-FROM Producto_Compra pc, Productos p
-WHERE p.CODIGO_BARRAS = productoid;
-
-IF promoid IS NOT NULL
-    UPDATE Promociones 
-    SET ps.UNIDADES_VENDIDAS = ps.UNIDADES_VENDIDAS + pc.CANTIDAD_PRODUCTO, ps.UNIDADES_DISPONIBLES = ps.UNIDADES_DISPONIBLES - pc.CANTIDAD_PRODUCTO 
-    FROM Producto_Compra pc, Promociones ps 
-    WHERE ps.NUM_PROM = promoid;
+INSERT INTO A_CARRITO
+VALUES (1,1000654987,1);
